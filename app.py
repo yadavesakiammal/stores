163944c18c72dd8fe9168cd01d1aaf5a3658b0e7
yadavesakiammal <your_email@gmail.com>
2026-04-13 11:30:34 +0530

    initial commit

--- app.py
+++ app.py
@@ -1601,5 +1601,5 @@ ADMIN_USERS_TEMPLATE = '''
 '''
 
 if __name__ == '__main__':
-    init_data()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
